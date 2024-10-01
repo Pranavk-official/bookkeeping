8b8b5c6cf6fd4165f7def472c449b1b639543ea5
Pranav K <pranavkcse@gmail.com>
2024-10-01 23:03:58 +0530

    feat: routes setup to app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
 import logger from "morgan";
 
+import bookRoutes from "./routes/bookRoutes.js";
+import borrowRoutes from "./routes/borrowRoutes.js";
+import libraryRoutes from "./routes/libraryRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
 
 // Middleware
@@ -9,6 +14,11 @@ app.use(express.json());
 app.use(logger("dev"));
 
 // Routes
+
+app.use("/api/books", bookRoutes);
+app.use("/api/borrows", borrowRoutes);
+app.use("/api/libraries", libraryRoutes);
+app.use("/api/users", userRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
