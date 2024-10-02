a6977a1744a0c1cb219641448615cf802f564a79
Pranav K <pranavkcse@gmail.com>
2024-10-02 09:11:47 +0530

    feat: added jwtUtilities

---
+++ src/utils/jwtUtils.js
@@ -0,0 +1,12 @@
+// src/utils/jwtUtils.js
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, role) => {
+  return jwt.sign({ userId, role }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
