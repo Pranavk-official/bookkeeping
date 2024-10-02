b81cf3d0f57b4a5597fa48e66b3fbe456a9b4674
Pranav K <pranavkcse@gmail.com>
2024-10-02 10:35:03 +0530

    minor spacing fix

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,6 @@ import { registerUser, loginUser } from "../controllers/userController";
 const router = express.Router();
 
 router.post("/register", registerUser);
-
 router.post("/login", loginUser);
 
 export default router;
