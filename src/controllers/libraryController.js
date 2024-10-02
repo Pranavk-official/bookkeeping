a42066d5ca32c1c31fd93103b45cf8a7c08a6b97
Pranav K <pranavkcse@gmail.com>
2024-10-02 10:33:44 +0530

    feat: created basic controller functions for library

--- src/controllers/libraryController.js
+++ src/controllers/libraryController.js
@@ -1,21 +1,59 @@
+import asyncHandler from "express-async-handler";
+
 export const getAllLibraries = async (req, res) => {
   res.send("getAllLibraries");
 };
 
-export const createLibrary = async (req, res) => {
-  res.send("createLibrary");
-};
+export const createLibrary = asyncHandler(async (req, res) => {
+  const library = new library(req.body);
+  await library.save();
+  res.status(201).json({
+    message: "Library created successfully",
+    library: {
+      id: library._id,
+      name: library.name,
+    },
+  });
+});
 
-export const getLibraryById = async (req, res) => {
-  res.send("getLibraryById");
-};
+export const getLibraryById = asyncHandler(async (req, res) => {
+  const library = await library.findById(req.params.id);
+  if (!library) {
+    res.status(404);
+    throw new Error("Library not found");
+  }
+  res.json({
+    id: library._id,
+    name: library.name,
+  });
+});
 
-export const updateLibrary = async (req, res) => {
-  res.send("updateLibrary");
-};
+export const updateLibrary = asyncHandler(async (req, res) => {
+  const library = await library.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  if (!library) {
+    res.status(404);
+    throw new Error("Library not found");
+  }
+  res.json({
+    message: "Library updated successfully",
+    id: library._id,
+    name: library.name,
+  });
+});
 
 export const deleteLibrary = async (req, res) => {
-  res.send("deleteLibrary");
+  const library = await library.findByIdAndDelete(req.params.id);
+
+  if (!library) {
+    res.status(404);
+    throw new Error("Library not found");
+  }
+  res.json({
+    message: "Library deleted successfully",
+  });
 };
 
 export const getLibraryInventory = async (req, res) => {
