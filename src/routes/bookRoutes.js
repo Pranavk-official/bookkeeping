f25f1bf08ee3e1a25e36ab48aa58aa49888758f6
Pranav K <pranavkcse@gmail.com>
2024-10-03 13:19:47 +0530

    feat: added route checking for necessary routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,18 +1,22 @@
+// src/routes/bookRoutes.js
 import express from "express";
-
 import {
   getAllBooks,
-  getBookById,
   createBook,
+  getBookById,
   updateBook,
   deleteBook,
 } from "../controllers/bookController.js";
-
 import auth from "../middleware/auth.js";
+import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
-router.route("/").get(auth, getAllBooks).post(auth, createBook);
+router
+  .route("/")
+  .get(auth, getAllBooks)
+  .post(auth, upload.single("image"), createBook);
+
 router
   .route("/:id")
   .get(auth, getBookById)
--- src/routes/libraryRoutes.js
+++ src/routes/libraryRoutes.js
@@ -1,23 +1,41 @@
 import express from "express";
-
 import {
   getAllLibraries,
-  getLibraryById,
   createLibrary,
+  getLibraryById,
   updateLibrary,
   deleteLibrary,
-} from "../controllers/libraryController";
-
-import auth from "../middleware/auth";
+  getLibraryInventory,
+  addBookToInventory,
+  removeBookFromInventory,
+} from "../controllers/libraryController.js";
+import auth from "../middleware/auth.js";
+import { authRole } from "../middleware/authRole.js";
 
 const router = express.Router();
 
-router.route("/").get(auth, getAllLibraries).post(auth, createLibrary);
+router
+  .route("/")
+  .get(auth, getAllLibraries)
+  .post(auth, authRole(["author", "admin"]), createLibrary);
 
 router
   .route("/:id")
   .get(auth, getLibraryById)
-  .put(auth, updateLibrary)
-  .delete(auth, deleteLibrary);
+  .put(auth, authRole(["author", "admin"]), updateLibrary)
+  .delete(auth, authRole(["author", "admin"]), deleteLibrary);
+
+router
+  .route("/:id/inventory")
+  .get(auth, getLibraryInventory)
+  .post(auth, authRole(["author", "admin", "librarian"]), addBookToInventory);
+
+router
+  .route("/:id/inventory/:bookId")
+  .delete(
+    auth,
+    authRole(["author", "admin", "librarian"]),
+    removeBookFromInventory
+  );
 
 export default router;
