4da8208784a0c2ebd0d1bba213ed6de92399dc3e
Pranav K <pranavkcse@gmail.com>
2024-10-03 13:18:31 +0530

    feat: middleware for uploading images using multer

---
+++ src/middleware/upload.js
@@ -0,0 +1,11 @@
+// src/middleware/upload.js
+import multer from "multer";
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // limit file size to 5MB
+  },
+});
+
+export default upload;
