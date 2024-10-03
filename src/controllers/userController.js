96d4478dc451d9b9a03a5288f6dbea88142bc363
Pranav K <pranavkcse@gmail.com>
2024-10-03 13:30:12 +0530

    refactor: changed the use of asyncHandler to try-catch for properly handling errors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,55 +1,85 @@
-import asyncHandler from "express-async-handler";
+// src/controllers/userController.js
+import User from "../models/user.js";
 import bcrypt from "bcryptjs";
-import User from "../models/user";
-import { generateToken } from "../utils/jwtUtils";
+import { getMessage } from "../utils/languageUtils.js";
+import { generateToken } from "../utils/jwtUtils.js";
 
-// TODO: use custom error handling with try catch
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
 
-export const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, role } = req.body;
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({
+        message: getMessage(
+          "allFieldsRequired",
+          req.headers["accept-language"]
+        ),
+      });
+    }
 
-  if (!name || !email || !password || !role) {
-    res.status(400);
-    throw new Error("Please add all fields");
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: getMessage(
+          "userAlreadyExists",
+          req.headers["accept-language"]
+        ),
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      message: getMessage(
+        "userRegisteredSuccessfully",
+        req.headers["accept-language"]
+      ),
+      userId: user._id,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: getMessage(
+        "errorRegisteringUser",
+        req.headers["accept-language"]
+      ),
+      error: error.message,
+    });
   }
+};
 
-  // Check if user exists
-  const userExists = await User.findOne({ email });
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({
+        message: getMessage(
+          "invalidCredentials",
+          req.headers["accept-language"]
+        ),
+      });
+    }
+
+    const token = generateToken(user._id, user.role);
+
+    res.json({
+      message: getMessage("loginSuccessful", req.headers["accept-language"]),
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: getMessage("errorLoggingIn", req.headers["accept-language"]),
+      error: error.message,
+    });
   }
-
-  // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  // Create user
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-    role,
-  });
-
-  await user.save();
-
-  res.status(201).json({
-    userId: user._id,
-    message: "User created successfully",
-  });
-});
-
-export const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-
-  if (!user && !(await bcrypt.compare(password, user.password))) {
-    res.status(401);
-    throw new Error("Invalid email or password");
-  }
-
-  const token = generateToken(user._id, user.role);
-  res.status(200).json({ token, message: "Login successful" });
-});
+};
